e5fd24b809760a893b81e4554079bea933cff320
JordanAleman <jordan.aleman.estudiante@gmail.com>
2020-03-08 19:03:38 +0100

    Created Alert Button in MainPage

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-header',
@@ -8,8 +9,22 @@ import { Component, OnInit, Input } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   @Input() title: string;
+  @Input() checkMainPage: string;
+  
 
-  constructor() { }
+  constructor(public alertController: AlertController) {}
+
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Tourist Treasure Hunt',
+      subHeader: 'Information Game',
+      message: '<p>Ready to <b><i>sightsee</i></b> and get to know iconic places playing treasure hunt?</p><p>This game consists of choosing one of the routes available on this page and following the directions to get to the destination</p><p>Upon arrival at the requested destination, a question with a series of answers will appear. Try to get the right answer!</p><p>At the end of the game a certain amount of points will be achieved. With so many points there can be prizemoney!</p>',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 
   ngOnInit() {}
 
