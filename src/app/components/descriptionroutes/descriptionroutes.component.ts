f73cc522091406d4d6cf4d1bdff068f792041b1f
JordanAleman <jordan.aleman.estudiante@gmail.com>
2020-03-12 11:59:42 +0000

    End design routes

--- src/app/components/descriptionroutes/descriptionroutes.component.ts
+++ src/app/components/descriptionroutes/descriptionroutes.component.ts
@@ -12,18 +12,150 @@ export class DescriptionroutesComponent implements OnInit {
 
   constructor(public descriptionRoute: AlertController) {}
 
-
   async descriptionRoute111() {
     const alert = await this.descriptionRoute.create({
       header: 'Santa Ana',
       subHeader: 'The oldest Something Something on Gran Canaria',
       // tslint:disable-next-line: max-line-length
-      message: 'orem ipsum dolor sit, amet consectetur adipisicing elit. Et, laudantium veritatis adipisci vel earum quam suscipit quasi. Totam, non sapiente!',
+      message: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, laudantium veritatis adipisci vel earum quam suscipit quasi. Totam, non sapiente!',
       buttons: ['OK']
     });
 
     await alert.present();
   }
+
+  async descriptionRoute112() {
+    const alert = await this.descriptionRoute.create({
+      header: 'Santa Ana',
+      subHeader: 'The oldest Something Something on Gran Canaria',
+      // tslint:disable-next-line: max-line-length
+      message: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, laudantium veritatis adipisci vel earum quam suscipit quasi. Totam, non sapiente!',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async descriptionRoute113() {
+    const alert = await this.descriptionRoute.create({
+      header: 'Santa Ana',
+      subHeader: 'The oldest Something Something on Gran Canaria',
+      // tslint:disable-next-line: max-line-length
+      message: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, laudantium veritatis adipisci vel earum quam suscipit quasi. Totam, non sapiente!',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async descriptionRoute121() {
+    const alert = await this.descriptionRoute.create({
+      header: 'Santa Ana',
+      subHeader: 'The oldest Something Something on Gran Canaria',
+      // tslint:disable-next-line: max-line-length
+      message: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, laudantium veritatis adipisci vel earum quam suscipit quasi. Totam, non sapiente!',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async descriptionRoute122() {
+    const alert = await this.descriptionRoute.create({
+      header: 'Santa Ana',
+      subHeader: 'The oldest Something Something on Gran Canaria',
+      // tslint:disable-next-line: max-line-length
+      message: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, laudantium veritatis adipisci vel earum quam suscipit quasi. Totam, non sapiente!',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async descriptionRoute123() {
+    const alert = await this.descriptionRoute.create({
+      header: 'Santa Ana',
+      subHeader: 'The oldest Something Something on Gran Canaria',
+      // tslint:disable-next-line: max-line-length
+      message: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, laudantium veritatis adipisci vel earum quam suscipit quasi. Totam, non sapiente!',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async descriptionRoute211() {
+    const alert = await this.descriptionRoute.create({
+      header: 'Santa Ana',
+      subHeader: 'The oldest Something Something on Gran Canaria',
+      // tslint:disable-next-line: max-line-length
+      message: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, laudantium veritatis adipisci vel earum quam suscipit quasi. Totam, non sapiente!',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async descriptionRoute212() {
+    const alert = await this.descriptionRoute.create({
+      header: 'Santa Ana',
+      subHeader: 'The oldest Something Something on Gran Canaria',
+      // tslint:disable-next-line: max-line-length
+      message: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, laudantium veritatis adipisci vel earum quam suscipit quasi. Totam, non sapiente!',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async descriptionRoute213() {
+    const alert = await this.descriptionRoute.create({
+      header: 'Santa Ana',
+      subHeader: 'The oldest Something Something on Gran Canaria',
+      // tslint:disable-next-line: max-line-length
+      message: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, laudantium veritatis adipisci vel earum quam suscipit quasi. Totam, non sapiente!',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async descriptionRoute221() {
+    const alert = await this.descriptionRoute.create({
+      header: 'Santa Ana',
+      subHeader: 'The oldest Something Something on Gran Canaria',
+      // tslint:disable-next-line: max-line-length
+      message: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, laudantium veritatis adipisci vel earum quam suscipit quasi. Totam, non sapiente!',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async descriptionRoute222() {
+    const alert = await this.descriptionRoute.create({
+      header: 'Santa Ana',
+      subHeader: 'The oldest Something Something on Gran Canaria',
+      // tslint:disable-next-line: max-line-length
+      message: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, laudantium veritatis adipisci vel earum quam suscipit quasi. Totam, non sapiente!',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  async descriptionRoute223() {
+    const alert = await this.descriptionRoute.create({
+      header: 'Santa Ana',
+      subHeader: 'The oldest Something Something on Gran Canaria',
+      // tslint:disable-next-line: max-line-length
+      message: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, laudantium veritatis adipisci vel earum quam suscipit quasi. Totam, non sapiente!',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
 
   ngOnInit() {}
 
